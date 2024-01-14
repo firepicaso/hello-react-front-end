584dbea35fb452860780a64e9f499dc1d2e9020e
Mustakim <m.m.kiron@gmail.com>
2024-01-14 09:10:09 +0600

    Configure store and greeting.js

---
+++ src/components/Greeting.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import getRandomGreeting from '../api';
+
+const Greeting = () => {
+  const randomGreeting = useSelector((state) => state.greetings.randomGreeting);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getRandomGreeting();
+  }, [dispatch]);
+
+  return (
+    <div className="greetings">
+      <h2>{randomGreeting}</h2>
+    </div>
+  );
+};
+
+export default Greeting;
